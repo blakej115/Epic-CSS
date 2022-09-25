61b63e03820e53466d9ecfc5d4d0952aed4dfd03
blakej115 <blakej115@gmail.com>
2022-09-24 21:58:04 -0400

    Built most basic setup of framework/website

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,18 @@
 <template>
-  <Tutorial />
+  <section class="section billboard">
+    <div class="container max-width-sm">
+      <div class="content text-align-center">
+        <h1>Epic CSS</h1>
+        <p>Epic CSS is a SASS boilerplate and CSS framework.</p>
+        <p>Copy the <code>assets/epic</code> folder into your project, edit the boilerplate/framework directly, and compile from scss to css.</p>
+        <p>Epic CSS is in it's very early stages. More features and documentation are coming soon.</p>
+        <div class="buttons justify-content-center">
+          <a class="button-primary" href="https://github.com/blakej115/Epic-CSS" target="_blank">Git Repo</a>
+          <a class="button-secondary" href="https://github.com/blakej115/Epic-CSS/archive/refs/heads/main.zip" target="_blank">Download Zip</a>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
